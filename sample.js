b9ddffd84dc03f36c0de9bb063591a98974b7b37
othman853 <anuar.yasser@gmail.com>
2017-03-03 02:26:14 -0300

    KISS

--- sample.js
+++ sample.js
@@ -1,6 +1,5 @@
-const { from, filter, map, each, present, not, uppercase } = require('./lib');
+const { from, filter, map, each } = require('./lib');
 const { log } = console;
-
 const alphabet = [
   'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
   'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
@@ -8,15 +7,12 @@ const alphabet = [
 
 const vowels = ['a', 'e', 'i', 'o', 'u'];
 
-const isVowel = present(vowels);
-const isConsonant = not(isVowel);
+const isVowel = l => vowels.includes(l);
+const isConsonant = l => !isVowel(l);
+const uppercase = l => l.toUpperCase();
 
-from(alphabet) (each, log);
+from(alphabet) (map, uppercase) (each, log);
 
-from(vowels) (each, log);
+from(alphabet) (filter, isVowel) (each, log);
 
-log(...(from(alphabet) (map, uppercase)) ());
-
-log(...(from(alphabet) (filter, isVowel)) ());
-
-log(...(from(alphabet) (filter, isConsonant) (map, uppercase)) ());
+from(alphabet) (filter, isConsonant) (map, uppercase) (each, log);
